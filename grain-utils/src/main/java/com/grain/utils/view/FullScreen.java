9f9afd1395b8c8751d2eda61e0f0ecfe5ac07485
GrainRain <h_liwei@qq.com>
2021-07-07 09:24:35 +0800

    v1.0.2

--- grain-utils/src/main/java/com/grain/utils/view/FullScreen.java
+++ grain-utils/src/main/java/com/grain/utils/view/FullScreen.java
@@ -22,18 +22,20 @@ public class FullScreen {
 
     /**
      * 隐藏状态栏和标题栏
+     * @param activity
      */
-    public static void setHideAllBar() {
-        setHideTitleBar();
-        setHideStatusBar();
-        hideNavKey();
+    public static void setHideAllBar(AppCompatActivity activity) {
+        setHideTitleBar(activity);
+        setHideStatusBar(activity);
+        hideNavKey(activity);
     }
 
     /**
      * 隐藏标题栏
+     * @param activity
      */
-    public static void setHideTitleBar() {
-        ActionBar actionBar = getActivity().getSupportActionBar();
+    public static void setHideTitleBar(AppCompatActivity activity) {
+        ActionBar actionBar = activity.getSupportActionBar();
         if (actionBar != null) {
             actionBar.hide();
         }
@@ -41,25 +43,26 @@ public class FullScreen {
 
     /**
      * 隐藏状态栏
+     * @param activity
      */
-    public static void setHideStatusBar() {
-        WindowManager.LayoutParams attrs = getActivity().getWindow().getAttributes();
+    public static void setHideStatusBar(Activity activity) {
+        WindowManager.LayoutParams attrs = activity.getWindow().getAttributes();
         attrs.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
-        getActivity().getWindow().setAttributes(attrs);
+        activity.getWindow().setAttributes(attrs);
     }
 
     /**
      * 隐藏底部按键
+     * @param activity
      */
-    public static void hideNavKey() {
+    public static void hideNavKey(Activity activity) {
         if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) {
-            View v = getActivity().getWindow().getDecorView();
+            View v = activity.getWindow().getDecorView();
             v.setSystemUiVisibility(View.GONE);
         } else if (Build.VERSION.SDK_INT >= 19) {
             //for new api versions.
-            View decorView = getActivity().getWindow().getDecorView();
-            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            View decorView = activity.getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
             decorView.setSystemUiVisibility(uiOptions);
         }
     }
--- grain-utils/src/main/java/com/grain/utils/view/SetScreenOrientation.java
+++ grain-utils/src/main/java/com/grain/utils/view/SetScreenOrientation.java
@@ -1,5 +1,6 @@
 package com.grain.utils.view;
 
+import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import static com.grain.utils.InitUtilsModule.getActivity;
 
@@ -12,16 +13,18 @@ public class SetScreenOrientation {
 
     /**
      * 强制横屏
+     * @param activity
      */
-    public static void horizontal() {
-        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    public static void horizontal(Activity activity) {
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 
     /**
      * 强制竖屏
+     * @param activity
      */
-    public static void portrait() {
-        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    public static void portrait(Activity activity) {
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
 }
